c723a0a0ebb168613178daee64b2f85ef7e6525f
acieabegum <asiyakuhu@gmail.com>
2024-11-18 12:23:57 +0600

    update

--- src/main/java/org/example/onlinevotingsystem/models/Option.java
+++ src/main/java/org/example/onlinevotingsystem/models/Option.java
@@ -1,8 +1,6 @@
 package org.example.onlinevotingsystem.models;
 
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,45 +8,34 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "poll")
+@Table(name = "options")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Poll {
+public class Option {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "PollID")
-    private int pollId;
+    @Column(name = "OptionID")
+    private int optionId;
 
     @Column(name = "Title", nullable = false)
     private String title;
 
-    @Column(name = "type")
-    private String type;
+    @Column(name = "VoteCount")
+    private int voteCount;
 
-    @Column(name = "PollDate")
-    private String pollDate;
-
-    @Column(name = "TotalVote")
-    private int totalVote;
+    @Column(name = "VotePercentage")
+    private int votePercentage;
 
     @ManyToOne
-    @JoinColumn(name = "AdminID", nullable = false)
-    private Admin admin;
-
-    @ManyToOne
-    @JoinColumn(name = "CategoryID", nullable = false)
-    private Category category;
-
-    @OneToMany(mappedBy = "poll", cascade = CascadeType.ALL)
-    private List<Option> options;
+    @JoinColumn(name = "PollID", nullable = false)
+    private Poll poll;
 
 }
--- src/main/java/org/example/onlinevotingsystem/repositories/OptionRepository.java
+++ src/main/java/org/example/onlinevotingsystem/repositories/OptionRepository.java
@@ -3,8 +3,8 @@ package org.example.onlinevotingsystem.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import org.example.onlinevotingsystem.models.Poll;
+import org.example.onlinevotingsystem.models.Option;
 
 @Repository
-public interface PollRepository extends JpaRepository<Poll, Integer> {
+public interface OptionRepository extends JpaRepository<Option, Integer> {
 }
