f21674a5d4fee57791a719958b9c3e6b4146fda1
Halimatus Sadia <sadia.00739@gmail.com>
2024-12-03 21:43:58 +0600

    registration fixed

--- src/main/java/org/example/onlinevotingsystem/FacadePattern/VotingSystemFacade.java
+++ src/main/java/org/example/onlinevotingsystem/FacadePattern/VotingSystemFacade.java
@@ -2,20 +2,23 @@ package org.example.onlinevotingsystem.FacadePattern;
 
 
 import org.example.onlinevotingsystem.models.User;
+import org.example.onlinevotingsystem.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class VotingSystemFacade implements IFacade{
+public class VotingSystemFacade implements IFacade {
 
-
+    @Autowired
+    private UserService userService;
 
     @Override
     @Transactional
     public void createUserAndAdminApprovalNotification(User user) {
 
         //creating user
-        //userService.registerUser(user);
+        userService.registerUser(user);
         //creating notification
         //notificationService.sendNotificationToAdmin(Constants.ROLE_ADMIN_USER_Approver);
     }
