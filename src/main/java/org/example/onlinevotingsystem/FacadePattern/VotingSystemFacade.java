439ca5ecc27efafe366fe213c87677c549bf9e02
Nafis Alam Khan <nafis.alam010@gmail.com>
2024-12-03 21:49:17 +0600

    facade

--- src/main/java/org/example/onlinevotingsystem/FacadePattern/VotingSystemFacade.java
+++ src/main/java/org/example/onlinevotingsystem/FacadePattern/VotingSystemFacade.java
@@ -1,22 +1,29 @@
 package org.example.onlinevotingsystem.FacadePattern;
 
 
+import org.example.onlinevotingsystem.models.Constants;
 import org.example.onlinevotingsystem.models.User;
+import org.example.onlinevotingsystem.services.NotificationService;
+import org.example.onlinevotingsystem.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class VotingSystemFacade implements IFacade{
 
-
+    @Autowired
+    private NotificationService notificationService;
+    @Autowired
+    private UserService userService;
 
     @Override
     @Transactional
     public void createUserAndAdminApprovalNotification(User user) {
 
         //creating user
-        //userService.registerUser(user);
+        userService.registerUser(user);
         //creating notification
-        //notificationService.sendNotificationToAdmin(Constants.ROLE_ADMIN_USER_Approver);
+        notificationService.sendNotificationToAdmin(Constants.ROLE_ADMIN_USER_Approver);
     }
 }
